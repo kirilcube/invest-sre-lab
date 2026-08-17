a28313ea5ee82b14da1288af57048251b922dbe5
nohnds <kiril302005@gmail.com>
2026-08-17 14:06:34 +0200

    logs + trading-api

--- deploy/postgres/init.sql
+++ deploy/postgres/init.sql
@@ -2,14 +2,14 @@ CREATE TABLE accounts (
     id SERIAL PRIMARY KEY,
     owner_id VARCHAR(50) NOT NULL,
     asset VARCHAR(10) NOT NULL,
-    balance NUMERIC(15, 2) NOT NULL DEFAULT 0.00,
+    balance BIGINT NOT NULL DEFAULT 0,
     UNIQUE(owner_id, asset)
 );
 
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
     reference_type VARCHAR(50),
-    reference_id INT,
+    reference_id INT, -- most of the times it's id of order
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -17,7 +17,7 @@ CREATE TABLE postings (
     id SERIAL PRIMARY KEY,
     transaction_id INT REFERENCES transactions(id),
     account_id INT REFERENCES accounts(id),
-    amount NUMERIC(15, 2) NOT NULL
+    amount BIGINT NOT NULL
 );
 
 CREATE TABLE orders (
@@ -26,13 +26,13 @@ CREATE TABLE orders (
     ticker VARCHAR(10) NOT NULL,
     side VARCHAR(4) NOT NULL,
     quantity INT NOT NULL,
-    price NUMERIC(15, 2) NOT NULL,
-    status VARCHAR(20) NOT NULL DEFAULT 'PENDING'
+    price BIGINT NOT NULL,
+    status VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    idempotency_key UUID UNIQUE NOT NULL
 );
 
 -- --- НАПОЛНЯЕМ ТЕСТОВЫМИ ДАННЫМИ ---
-
-INSERT INTO accounts (id, owner_id, asset, balance) VALUES (1, 'user_1', 'USD', 10000.00);
-INSERT INTO accounts (id, owner_id, asset, balance) VALUES (2, 'broker_main', 'USD', 0.00);
-INSERT INTO accounts (id, owner_id, asset, balance) VALUES (3, 'user_1', 'AAPL', 0.00);
-INSERT INTO accounts (id, owner_id, asset, balance) VALUES (4, 'broker_main', 'AAPL', 50.00);
+INSERT INTO accounts (id, owner_id, asset, balance) VALUES (1, 'service', 'USD', -1000000);
+INSERT INTO accounts (id, owner_id, asset, balance) VALUES (2, 'service', 'AAPL', 0);
+INSERT INTO accounts (id, owner_id, asset, balance) VALUES (4, 'user_1', 'USD', 1000000);
+INSERT INTO accounts (id, owner_id, asset, balance) VALUES (4, 'user_1', 'AAPL', 0);
--- go.mod
+++ go.mod
@@ -4,10 +4,16 @@ go 1.26.4
 
 require (
 	github.com/go-chi/chi/v5 v5.3.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.10.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/klauspost/compress v1.19.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.28 // indirect
+	github.com/segmentio/kafka-go v0.4.51 // indirect
+	github.com/twmb/franz-go v1.21.6 // indirect
+	github.com/twmb/franz-go/pkg/kmsg v1.13.1 // indirect
 	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/text v0.41.0 // indirect
 )
